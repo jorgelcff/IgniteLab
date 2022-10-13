271dcd17ed2ce0056fa19030963964cdfaf7b341
Jorge Freitas <80436467+jorgelcff@users.noreply.github.com>
2022-10-13 04:26:14 -0300

    add routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {App} from './App'
+import {BrowserRouter} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import { Private } from './pages/Private/Private';
+import { Register } from './pages/Register/Register';
+import { Login } from './Login';
+import { AuthProvider } from './contexts/auth/AuthProvider';
+import { RequireAuth } from './contexts/auth/RequireAuth';
+
+function App() {
+  return(
+    <Routes>
+    <Route path='/' element={<Login/>}/>
+    <Route path='/private' element={<RequireAuth><Private/></RequireAuth>}/>
+    <Route path='/register' element={<Register/>}/>
+  </Routes>
+  )
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 )
